02d70757301ebe39ff997764bf5cdebd14bfa312
Rafael Farsura <rafaelfarsura@gmail.com>
2025-04-21 18:25:40 -0300

    Implement password hashing in user creation endpoint

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,6 +9,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UsersService {
   constructor(
@@ -16,7 +18,7 @@ export class UsersService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<Omit<User, 'password'>> {
     const { email, password, name } = createUserDto;
 
     if (await this.userRepository.findOne({ where: { email } }))
@@ -24,30 +26,57 @@ export class UsersService {
         `User: ${name}, email: ${email} already exists`,
       );
 
-    const user: User = this.userRepository.create({ email, password, name });
-    if (user) return await this.userRepository.save(user);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    throw new BadRequestException(
-      `User: ${name}, email: ${email} could not be created`,
-    );
+    const user: User = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      name,
+    });
 
-    /// TODO need implement passwd encryption and JWT
+    try {
+      const savedUser = await this.userRepository.save(user);
+      const { password: _, ...result } = savedUser;
+      return result;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Email already registered.');
+      } else {
+        console.error('Erro ao salvar usuário:', error);
+
+        throw new BadRequestException(
+          'Error creating user.\nDetails:' + error.message,
+        );
+      }
+
+      /// TODO need implement and JWT
+    }
   }
 
-  findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async findAll(): Promise<Omit<User, 'password'>[]> {
+    const users = await this.userRepository.find();
+
+    return users.map((user) => {
+      const { password: _, ...result } = user;
+      return result;
+    });
   }
 
-  async findOneById(id: string): Promise<User | null> {
+  async findOneById(id: string): Promise<Omit<User, 'password'> | null> {
     const user = await this.userRepository.findOneBy({ id });
 
-    if (user) return user;
+    if (!user) return null;
 
-    throw new NotFoundException(`User: ${id} not found`);
+    const { password: _, ...result } = user;
+
+    return result;
   }
 
   async findOneByEmail(email: string): Promise<User | null> {
-    const user = await this.userRepository.findOneBy({ email });
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'email', 'name', 'password', 'createdAt', 'updatedAt'],
+    });
 
     if (user) return user;
 
