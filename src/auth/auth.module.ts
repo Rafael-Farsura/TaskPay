15844d1a6ccd0590caf9d01cbd6cc18f4c276df4
Rafael Farsura <rafaelfarsura@gmail.com>
2025-04-22 17:57:54 -0300

    Create bcrypt abstract class and hashing service
    Also improve catch error in bootstrap, add respectively providers in auth module, exclude unnecessary files/folders, add some packages that will be used

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+import { HashingServiceProtocol } from './hashing/hashing.service';
+import { BCryptService } from './hashing/bcrypt.service';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+    {
+      provide: HashingServiceProtocol,
+      useClass: BCryptService,
+    },
+  exports: [AuthService, HashingServiceProtocol],
 })
 export class AuthModule {}
